5ed7c650cdae5dc3335af453ee99a117dfff74bd
Gabriel Rigon <gabrielrigon@outlook.com>
2019-04-14 11:43:08 -0300

    feat(base): adds arkit scene view and debug options

--- ARKit2Playground/ViewController.swift
+++ ARKit2Playground/ViewController.swift
@@ -1,20 +1,28 @@
 //
 //  ViewController.swift
-//  ARKit2Playground
+//  ARKit 2 Playground
 //
 //  Created by Gabriel on 14/04/19.
 //  Copyright © 2019 Gabriel. All rights reserved.
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
-
+  let configuration = ARWorldTrackingConfiguration()
+  
+  @IBOutlet weak var sceneView: ARSCNView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    self.defineSceneView()
   }
-
-
+  
+  // - MARK: Methods
+  
+  func defineSceneView() {
+    self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+    self.sceneView.session.run(self.configuration)
+  }
 }
-
